93fabe577741711029c17ebd18faa3bd0add2081
Pallav Vyas <pallavvyas2000@gmail.com>
2022-03-19 04:58:55 +0530

    Final Build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { useState } from 'react';
+import Alerts from './components/Alerts';
 
 function App() {
+  const [mode, setmode] = useState('light')
+  const [alert, setalert] = useState(null)
+  const showAlert = (message, type) =>{
+    setalert({
+      message : message,
+      type : type
+    })
+  }
+  const toggleMode = () =>{
+      if(mode === 'light')
+        {setmode('dark');
+        document.body.style.backgroundColor = '#394046';
+        }
+      else
+        {setmode('light');
+        document.body.style.backgroundColor = 'white';}
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="TextUtils" mode = {mode} toggleMode = {toggleMode}/>
+    <Alerts/>
+    <div className="container">
+    <TextForm heading="Enter your text here :" mode = {mode}/>
     </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let UpText = text.toUpperCase();
+    setText(UpText);
+  };
+
+  const handleLowClick = () => {
+    let LowText = text.toLowerCase();
+    setText(LowText);
+  };
+
+  const handleClrText = () => {
+    let ClrText = "";
+    setText(ClrText);
+  };
+
+  const handleExtraSpaces = () => {
+    let xtraSpaceText = text.split(/[ ]+/);
+    setText(xtraSpaceText.join(" "));
+  };
+
+  const handleRemSpace = () => {
+    let remSpaceText = text.split(" ").join("");
+    setText(remSpaceText);
+  };
+
+  const handleCopy = () => {
+    var text = document.getElementById("txtBox")
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="container" style ={{color : props.mode === 'dark'? '#d9d9d9':'#394046'}}>
+        <h1>{props.heading}</h1>
+        <div className={`mb-4`}>
+          <textarea
+            className="form-control mt-4"
+            value={text}
+            onChange={handleOnChange}
+            style = {{backgroundColor : props.mode === 'dark'? '#394046':'white', color : props.mode === 'dark'? 'white':'black'}}
+            id="txtBox"
+            rows="8"
+            />{" "}
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleRemSpace}>
+          Remove Spaces
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClrText}>
+          Clear text
+        </button>
+      </div>
+      <div className="container mt-4" style ={{color : props.mode === 'dark'? '#d9d9d9':'#394046'}}>
+        <h2>Your text summary :</h2>
+          <p>
+            Word Count : {text.split(" ").length}
+            <br></br> Character Count :{text.length}
+          </p>
+          <p>
+            {0.008 * text.split(" ").length} Minute Read
+          </p>
+          <h2>Preview</h2>
+          <div className="container" style ={{color : props.mode === 'dark'? '#a9a9a9':'#5a5a5a'}}>
+          <p>{text.length>0?text:"Enter text to preview..."}</p>
+          </div>
+      </div>
+    </>
+  );
+}
+
+TextForm.propTypes = { heading: PropTypes.string };
